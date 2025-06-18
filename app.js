5dfa6757ff598847dae2905c459e8c8ae368e2cd
ramani-3528 <ramaneshwari3528@gmail.com>
2025-06-18 21:10:59 +0530

    re

--- app.js
+++ app.js
@@ -1,44 +1,44 @@
 let myString = "  Practice makes a man perfect.  ";
 
-document.getElementById("show").onclick = function() {
+document.getElementById("show").onclick = () => {
     alert("Current String: " + myString);
 };
 
-document.getElementById("length").onclick = function() {
+document.getElementById("length").onclick = () => {
     alert("Length: " + myString.length);
 };
 
-document.getElementById("replace").onclick = function() {
+document.getElementById("replace").onclick = () => {
     let result = myString.replace("man", "person");
     alert("Replaced: " + result);
 };
 
-document.getElementById("uppercase").onclick = function() {
+document.getElementById("uppercase").onclick = () => {
     alert("Uppercase: " + myString.toUpperCase());
 };
 
-document.getElementById("lowercase").onclick = function() {
+document.getElementById("lowercase").onclick = () => {
     alert("Lowercase: " + myString.toLowerCase());
 };
 
-document.getElementById("repeat").onclick = function() {
+document.getElementById("repeat").onclick = () => {
     alert("Repeated: " + myString.repeat(2));
 };
 
-document.getElementById("search").onclick = function() {
+document.getElementById("search").onclick = () => {
     alert("Position of 'man': " + myString.search("man"));
 };
 
-document.getElementById("concatination").onclick = function() {
+document.getElementById("concatination").onclick = () => {
     let result = myString.concat(" Keep learning!");
     alert("Concatenated: " + result);
 };
 
-document.getElementById("split").onclick = function() {
+document.getElementById("split").onclick = () => {
     let parts = myString.split(" ");
     alert("Split: " + parts.join(" | "));
 };
 
-document.getElementById("trim").onclick = function() {
+document.getElementById("trim").onclick = () => {
     alert("Trimmed: " + myString.trim());
 };
